14a44df315076ee5c0d3647e17299f45883389c3
Puffin&TurtleSoftware <puffinandturtle@gmail.com>
2016-04-25 22:31:09 +0100

    Initial Commit

---
+++ HeightMapTests/HeightMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  HeightMapTests.swift
+//  HeightMapTests
+//
+//  Created by Oliver Dew on 25/04/2016.
+//  Copyright © 2016 Salt Pig. All rights reserved.
+//
+
+import XCTest
+@testable import HeightMap
+
+class HeightMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
